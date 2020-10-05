a5c973310eb71005340c0962d964e7133eab6127
laffed <roarklaffed@gmail.com>
2020-10-05 15:48:47 -0500

    topnav styling

--- client/src/Components/TopNav/TopNav.jsx
+++ client/src/Components/TopNav/TopNav.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './topnav.style.scss';
+import Logo from '../../resources/logo-symbol.svg';
 
 function TopNav() {
 
   return (
     <div className="topnav-wrapper">
-      <h1>Logo</h1>
+      <img src={Logo} alt="Helpful Human" />
       <input type="text" placeholder='Search' />
     </div>
   );
