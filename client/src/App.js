e38c89d3e45ccf2341f7a4c869d5991301c80381
laffed <roarklaffed@gmail.com>
2020-10-05 16:35:34 -0500

    overview strategy on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,24 @@ import Routes from './Routes';
 import SideNav from './Components/SideNav/SideNav';
 import TopNav from './Components/TopNav/TopNav';
 
+/*
+OVERVIEW:
+
+Main: 
+Pagination 8 pages of 12 colors each - 96 segments of ranges
+Total number of hex color is 16,777,216 possible colors
+Therefore each color Main color will be about 174,762 hexidecimals apart
+
+SideNav:
+Review the design spec 
+  Option 1: 
+  Each base color Routes to detail page with base color hexcode
+
+  Option 2: 
+  Each base color shows a page in main (pagination) of all hues within the base color range of hex colors
+  
+*/
+
 function App() {
   return (
     <div className="App">
